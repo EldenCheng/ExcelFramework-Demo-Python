b1e65147b691b220433a1fd2431f466e2eae1f5b
EldenZheng <elden.zheng@wesoft.com>
2017-08-03 17:31:59 +0800

    1234

--- Test/Excel_x_Test.py
+++ Test/Excel_x_Test.py
@@ -1,24 +1,28 @@
 from Common.Excel_x import Excel
 
-excel = Excel(r"..\TestCaseData\Kerry_data_2c.xlsx")
+#excel = Excel(r"..\TestCaseData\Kerry_data_2c.xlsx")
 
-excel.Select_Sheet_By_Name("1")
+excel = Excel(r"D:\百度云同步盘\项目\自动化测试\Kerry\KerryDemo\TestReport\2017-08-02_182545\Report_2017-08-02_182545.xlsx")
 
-print(excel.Get_Row_Numbers())
+excel.Select_Sheet_By_Name("result")
 
-print(excel.Get_All_Values_By_ColName("ID"))
+print(excel.Get_Value_By_ColName("Screen capture", 1))
 
-print(excel.Get_Value_By_ColName("ID", 1))
+#print(excel.Get_Row_Numbers())
 
-print(excel.Get_Value_By_ColName("PW", 1, r"D:\VBA"))
+#print(excel.Get_All_Values_By_ColName("ID"))
 
-report= Excel(r"..\TestReport\Kerry_data_2w.xlsx")
+#print(excel.Get_Value_By_ColName("ID", 1))
 
-report.Select_Sheet_By_Name("1")
+#print(excel.Get_Value_By_ColName("PW", 1, r"D:\VBA"))
+
+#report= Excel(r"..\TestReport\Kerry_data_2w.xlsx")
+
+#report.Select_Sheet_By_Name("1")
 
 #report.Set_Sheet_Name("1","5")
 
 #report.Set_Value_By_ColName(r'=HYPERLINK("D:\百度云同步盘\项目\自动化测试\Kerry\KerryDemo\TestReport\2017-08-02_145238\TC4\Pass\TC4_Dataset_1_Step_Pass.png","TC4_Dataset_1_Step_Pass.png")', "Screen capture", 1)
 
-report.Save_Excel()
+#report.Save_Excel()
 
