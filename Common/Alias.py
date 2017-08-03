046b55bcd57f1187fcf5106034b905d1142fdf6b
EldenZheng <elden.zheng@wesoft.com>
2017-08-03 12:37:37 +0800

    Add some of the PO Matain Page element exp

--- Common/Alias.py
+++ Common/Alias.py
@@ -3,16 +3,21 @@
 LoginPageAlias_CSS = {'ID_Field': 'userId',
                       'PW_Field': 'password',
                       'Login_Btn': r'btn_login-btnEl',
-                      'Login_Error_Prompt': "viewport-message-area-body",
-                      'Login_Error_Prompt_expression': "div#%s div span",
+                      'Login_Error_Prompt': "div#viewport-message-area-body div span",
                       'Verfidation_Code_Field': "'input#textfld_vc-inputEl'"}
 
-StartPageAlias_CSS = {'Login_UserName': 'LogonUserName-body',
-                      'Login_UserName_expression': r"div#%s div",
+StartPageAlias_CSS = {'Login_UserName': r"div#LogonUserName-body div",
                       'Menu_Invitation': r'label-1030',
+                      'Menu-PO Maintenance': r"label-1054",
                       'Logout_Btn': r"btn_logout-btnEl"}
 
-InvitationPageAlias_CSS = {'Contact_Person': "contactPerson",
-                           'Phone_Number': "phoneNo",
+InvitationPageAlias_CSS = {'Contact Person': "contactPerson",
+                           'Phone Number': "phoneNo",
                            'Submit_Btn': r"btn_submit-btnEl",
                            'Email_Error_icon': "td#txfld_companyEmail-sideErrorCell"}
+
+POMaintenancePageAlias_CSS = {'Order Type': "tr#ext-gen1256 td input",
+                             'Purchase Order': 0,
+                             'Sales Order': 1,
+                              '0': 0,
+                              '1': 1}
